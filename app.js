40d182274a7c363ae1c560a3c7f29c817f6f5cbb
unknown <cherif.bellahouel161@gmail.com>
2023-12-14 13:25:04 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+require('dotenv').config();
+require('./database');
+const cors = require('cors');
+const productRouter = require('./router/product.router');
+
+const { PORT } = process.env || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/products', productRouter);
+
+app.get('/', (req, res, next) => {
+    res.status(200).json({message: 'Bienvenue sur mon API'});
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).json({ status: 'error', message: err });
+});
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'not found: check the url !' });
+});
+
+app.listen(PORT, () => {
+    console.log(`=> server lauched on port : ${PORT}`);
+});
---
+++ router/product.router.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+const db = require('../database/db');
+
+router.get('/', async (req, res, next) => {
+    try {
+        const sql = "SELECT * FROM Products";
+        db.query(sql, (err, results) => {
+            if (err) {
+                console.log('Erreur lors de l\'affichage des produits');
+                res.status(500).json({ message : err })
+            } else {
+                res.status(200).json(results);
+            }
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/:productId', async (req, res, next) => {
+    try {
+        const productId = req.params.productId;
+        const sql = "SELECT * FROM Products WHERE id=?";
+        db.query(sql, [productId], (err, results) => {
+            if (err) {
+                console.log('Erreur lors du get du produit');
+                res.status(500).json({ message : err })
+            } else {
+                res.status(200).json(results);
+            }
+        }
+        )
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        const { title, price, description } = req.body;
+        
+        console.log(req.body);
+        const checkProduct = "SELECT * FROM Products WHERE title=?";
+        db.query(checkProduct, [title], (err, results) => {
+            if(results){
+                res.status(400).json({ message:  'Ce produit existe déjà'});
+            }
+        });
+    
+        const sql = "INSERT INTO Products(title, price, description) VALUES(?, ?, ?)";
+    
+        db.query(sql, [title, price, description], (err, results) => {
+            if (err) {
+                console.log('Erreur lors de l\'ajout d\'un produit');
+                res.status(500).json({ message : err })
+            } else {
+                res.status(200).json(results);
+            }
+        })
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.put(
+    '/:productId',
+    async (req, res, next) => {
+        try {
+            // CODE ICI
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.delete(
+    '/:productId',
+    async (req, res, next) => {
+        try {
+            // CODE ICI
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+module.exports = router;
